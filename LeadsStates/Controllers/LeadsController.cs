34960a321a92bcb78c7f279ae7c876bec86dc8aa
h00rzahid <hoor.devpel@gmail.com>
2021-02-18 00:06:33 +0500

    create lead page

---
+++ LeadsStates/Controllers/LeadsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LeadsStates.Controllers
+{
+    public class LeadsController : Controller
+    {
+        // GET: Leads
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult create_leads()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        public ActionResult lead_detail()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+    }
+}
